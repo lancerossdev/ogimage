db3e7221c9f225f22dcbaacac217024ab2fecb7d
Lance Ross <102563271+lancerossdev@users.noreply.github.com>
2022-11-10 16:49:39 +0800

    i forgot the async on function

--- netlify/edge_functions/og.tsx
+++ netlify/edge_functions/og.tsx
@@ -1,7 +1,7 @@
 import React from "https://esm.sh/react@18.2.0";
 import { ImageResponse } from "https://deno.land/x/og_edge@0.0.2/mod.ts";
 
-export default function handler(req: Request) {
+export default async function handler(req: Request) {
   const url = new URL(req.url);
   const params = new URLSearchParams(url.search);
   const title = params.get("title") ?? "Powered by Netlify Edge Functions";
